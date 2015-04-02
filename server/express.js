0459a39290f50b17c83247b6f9e7a738bdeae545
J0hnRoger <jonathan.roger@free.fr>
2015-04-03 01:52:11 +0200

    Ajout design calendrier + materialize + animate

--- server/express.js
+++ server/express.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+/*
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,4 +13,8 @@ app.listen(3002);
 
 app.get('/', function(req, res, next) {
   return "hello world";
-});
+});*/
+
+app.listen(3000); //the port you want to use
+
+app.use(express.static(__dirname + '/'));
